8724b2f39a69256931f12436d33034a8115e4439
Ismael <ismaellopez2905@gmail.com>
2026-06-01 22:21:50 -0300

    limpieza de variables de entorno

--- env.d.ts
+++ env.d.ts
@@ -1,20 +1,6 @@
 declare namespace NodeJS {
     interface ProcessEnv {
         DATABASE_URL: string;
-        DATABASE_URL_UNPOOLED: string;
-        PGHOST: string;
-        PGHOST_UNPOOLED?: string;
-        PGUSER: string;
-        PGDATABASE?: string;
-        PGPASSWORD: string;
-        POSTGRES_URL: string;
-        POSTGRES_URL_NON_POOLING: string;
-        POSTGRES_USER: string;
-        POSTGRES_HOST: string;
-        POSTGRES_PASSWORD: string;
-        POSTGRES_DATABASE: string;
-        POSTGRES_URL_NO_SSL: string;
-        POSTGRES_PRISMA_URL: string;
 		SELLER_API_KEY: string;
     }
 }
